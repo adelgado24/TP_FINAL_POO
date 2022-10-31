39dbca90dfad6d3f3b45d5b46075c4f1f267e61d
adelgado24 <adelgado24@ucema.edu.ar>
2022-10-31 20:27:33 -0300

    anotaciones plots

--- testeos.py
+++ testeos.py
@@ -5,25 +5,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import datetime as dt
 
-df = pd.read_csv("datos/dat-ab-append.csv")
 
-#groupby tipo transporte
+# Cargamos los datos
 
-gr = df.groupby("TIPO_TRANSPORTE")
+df = {'fecha': ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05'], 'casos': [313754198, 306069353, 222294728, 59187842, 77753791]}
+df = pd.DataFrame(df)
 
-definitvo = gr["CANTIDAD"].agg({"count","sum","mean"}).reset_index()
+#Loc min value index
 
-#make 3 subpie charts of definitivo, with matplotlib
+min_value = df['casos'].min()
+min_index = df['casos'].idxmin()
 
-fig, ax = plt.subplots(1,3,figsize=(15,5))
-
-ax[0].pie(definitvo["count"],labels=definitvo["TIPO_TRANSPORTE"],autopct="%1.1f%%")
-ax[0].set_title("Cantidad de viajes por tipo de transporte")
-ax[1].pie(definitvo["sum"],labels=definitvo["TIPO_TRANSPORTE"],autopct="%1.1f%%")
-ax[1].set_title("Cantidad de pasajeros por tipo de transporte")
-ax[2].pie(definitvo["mean"],labels=definitvo["TIPO_TRANSPORTE"],autopct="%1.1f%%")
-ax[2].set_title("Cantidad promedio de pasajeros por tipo de transporte")
-#add percentage to pie charts
-ax[0].axis("equal")
-ax[1].axis("equal")
-plt.show()
+print(min_value, min_index)
