052486f24d5d21d3982d50f97a92c584fa43e248
adelgado24 <adelgado24@ucema.edu.ar>
2022-11-05 15:09:23 -0300

    formateo markdown

--- testeos.py
+++ testeos.py
@@ -5,13 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import datetime as dt
 
+#get first index of the dataframe
 
-# Cargamos los datos
 
-#make 2 subplots
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
-#plot 1st
-ax1.plot(df['Fecha'], df['Casos'], color='red')
-#add xlabel to ax1
-ax1.set_xlabel('Fecha')
